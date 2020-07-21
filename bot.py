31b776cb606b4993ab525e85ff426fdd7442d48d
Jeffrey Yurkiw <jyurkiw@yahoo.com>
2020-07-20 22:07:46 -0700

    Formatting wastable header.

--- bot.py
+++ bot.py
@@ -5,22 +5,20 @@ from bot.cyberpunk_bot import CyberpunkBotClient
 from bot.table_roller import getTableRoller
 from bot import getSkillRoller
 from commands.exit_command import ExitCommand
-from commands.wastable_commands import GenerateWastableStatsCommand
 from commands.wastable_commands import GenerateWastableCommand
 
 from CP2020_Discord_Bot_API.api.util import CPDataHandler
 from CP2020_Discord_Bot_API.api.lifepath.siblings import SiblingsModule
 
-
 if __name__ == "__main__":
     load_dotenv()
     token = os.getenv("BOT_TOKEN")
     dataDir = os.getenv("DATA_DIR")
+    dbName = os.getenv("DB_NAME")
 
     # Build the bot
     bot = CyberpunkBotClient()
     bot.registerCommandObject(ExitCommand())
-    bot.registerCommandObject(GenerateWastableStatsCommand())
-    bot.registerCommandObject(GenerateWastableCommand())
+    bot.registerCommandObject(GenerateWastableCommand(dbName))
 
     bot.run(token)
--- commands/wastable_commands.py
+++ commands/wastable_commands.py
@@ -6,6 +6,8 @@ from collections import namedtuple
 from random import choice
 from random import randint
 
+from io import StringIO
+
 from .base_command import BaseSyncCommand
 from CP2020_Discord_Bot_API.api.stats import StatBlock
 from CP2020_Discord_Bot_API.api.equipment.weapons import WeaponsRoller
@@ -13,75 +15,52 @@ from CP2020_Discord_Bot_API.api.lifepath import LifepathRoller
 from CP2020_Discord_Bot_API.api.skills import SkillRoller
 
 
-def AddLineBreak(e):
-    e.add_field(name="\u200B", value="\u200B", inline=False)
-
-
-def AddWastableStats(e, sb):
-    for stat in sb.stats:
-        e.add_field(name=stat.name, value=stat.value, inline=True)
-
-
-def AddTableResults(e, results):
-    for result in results:
-        e.add_field(name=result.name, value=result.value, inline=True)
-
-
-def AddSkills(e, roleName, skills):
-    valueFormat = (
-        "{0:>" + str(max([len(skill.value) for skill in skills])) + "} +{1}"
-    )
-    skillTable = "```"
-    skillTable += "\n".join(
-        [valueFormat.format(skill.value, skill.count) for skill in skills]
-    )
-    skillTable += "```"
-
-    e.add_field(
-        name="Role: {0}".format(roleName), value=skillTable, inline=False
-    )
-
-
-def AddWeapons(e, weaponList):
-    wtuple = namedtuple("wtuple", ["width", "key"])
-    weaponColWidths = defaultdict(lambda: 0)
-    for t in [wtuple(width=len(k), key=k) for w in weaponList for k in w] + [
-        wtuple(width=max(weaponColWidths[k], len(str(w[k]))), key=k)
-        for w in weaponList
-        for k in w
-    ]:
-        weaponColWidths[t.key] = t.width
-
-    weaponColFormat = "{{name:^{name}}}\t{{type:^{type}}}\t{{wa:^{wa}}}\t{{concealability:^{concealability}}}\t{{availability:^{availability}}}\t{{damage}}\({{ammo_type}}\)\t{{shots:^{shots}}}\t{{rate_of_fire:^{rate_of_fire}}}\t{{reliability:^{reliability}}}\t{{range:^{range}}}".format(
-        **weaponColWidths
-    )
-
-    weaponsTable = "```"
-    weaponsTable += "\n".join([weaponColFormat.format(**w) for w in weaponList])
-    weaponsTable += "```"
-
-    e.add_field(name="Weapons", value=weaponsTable, inline=False)
-
-
 class GenerateWastableCommand(BaseSyncCommand):
-    def __init__(self, tableRoller, skillRoller):
+    def __init__(self, dbName):
         super().__init__("gen-w")
-        self.weaponsRoller = WeaponsRoller()
-        self.lifepathRoller = LifepathRoller()
-        self.skillsRoller = SkillRoller()
+        self.weaponsRoller = WeaponsRoller(dbName)
+        self.lifepathRoller = LifepathRoller(dbName)
+        self.skillsRoller = SkillRoller(dbName)
 
     def runCommand(self, arguments, message=None):
-        sb = StatBlock().generateRandom()
-        sk = self.skillsRoller.rollRandomRole(sb["int"] + sb["ref"], points=40)
+        sb = StatBlock().generateRandom().toDict()
+        role, sks = self.skillsRoller.rollRandomRole(
+            sb["int"] + sb["ref"], points=40
+        )
         lp = self.lifepathRoller.rollLifepath()
-        wps = [self.randomWeapon() for n in range(0, randint(1, 3))]
+        wps = [
+            self.weaponsRoller.getRandomWeapon()
+            for n in range(0, randint(1, 3))
+        ]
 
-        # responses.append(response)
+        output = StringIO()
+        self.formatHeader(output, role=role)
 
-        return "Stuff goes\n\t\t\tHere: ---> <---"
+        return output.getvalue()
 
-    def randomWeapon(self):
-        # fix this
-        return self.weaponsRoller.getRandomWeapon(
-            choice(self.weaponsRoller.getWeaponCategories())
+    def formatHeader(self, output, handle=None, role=None):
+        if not role:
+            raise Exception("Role is required.")
+
+        output.write("Handle: Not Yet Implemented\n")
+        output.write("Role: " + role)
+
+    def formatWeapons(self, output, weaponList):
+        wtuple = namedtuple("wtuple", ["width", "key"])
+        weaponColWidths = defaultdict(lambda: 0)
+        for t in [
+            wtuple(width=len(k), key=k) for w in weaponList for k in w
+        ] + [
+            wtuple(width=max(weaponColWidths[k], len(str(w[k]))), key=k)
+            for w in weaponList
+            for k in w
+        ]:
+            weaponColWidths[t.key] = t.width
+
+        weaponColFormat = "{{name:^{name}}}\t{{type:^{type}}}\t{{wa:^{wa}}}\t{{concealability:^{concealability}}}\t{{availability:^{availability}}}\t{{damage}}\({{ammo_type}}\)\t{{shots:^{shots}}}\t{{rate_of_fire:^{rate_of_fire}}}\t{{reliability:^{reliability}}}\t{{range:^{range}}}".format(
+            **weaponColWidths
+        )
+
+        output.write(
+            "\n".join([weaponColFormat.format(**w) for w in weaponList])
         )
